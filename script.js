df8b3340d55d5247a7a50921bce6265d1d6d67a7
codelenov <codelenovo@tutanota.com>
2024-05-17 19:00:43 +0200

    ADD temporary comments

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ window.onscroll = function() {
 	}
 	previousPosition = currentPosition;
 }
+
+
+page navbar - aside
+pages navbar - header
+all css in 1
+make one mediaquery for all css attributes
+change font-family
+resume - a4
+resume as link from #about (show resume)
+articles
+links
+copy header, aside, footer - in all html (and next fix aside in each html)
+
+!!!!!!!!! add addEventListener with header to bottom - for all links in all aside in all files (class)
+!!! make padding and margin in em (not px)
+hide navbar on scroll
+responsive header
+responsive topnav
+
+fixed sidebar with full height
+responsive sidebar
+
+links from roadmap 
