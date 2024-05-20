6c2650fbc531f5a98235f9d627e7af15e72f25a4
codelenov <codelenovo@tutanota.com>
2024-05-20 18:30:00 +0200

    CHANGE more code

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let previousPosition = window.pageYOffset;
+let x = window.matchMedia('(max-width: 700px)');
 
 window.onscroll = function() {
 	const currentPosition = window.pageYOffset;
 	if (previousPosition < currentPosition) {
-		document.querySelector('header').style.top = '-3em';
+		if (x.matches) {
+			document.querySelector('header').style.top = '-4.5em';
+		} else {
+			document.querySelector('header').style.top = '-3em';
+		}
 	} else {
 		document.querySelector('header').style.top = '0';
 	}
